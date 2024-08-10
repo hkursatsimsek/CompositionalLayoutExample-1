88aaad7f6294e21e0539c8d8d7a6bed2c06473df
Kürşat Şimşek <kursatsimsek@192.168.1.102>
2024-08-10 22:50:55 +0300

    Second compositional layout added.

--- CompositionalLayoutExample-1/ViewController.swift
+++ CompositionalLayoutExample-1/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDataSource {
     
-    private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: ViewController.createLayout())
+    private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: .init())
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,10 +18,20 @@ class ViewController: UIViewController, UICollectionViewDataSource {
         collectionView.register(MyCollectionViewCell.self, forCellWithReuseIdentifier: MyCollectionViewCell.identifier)
         collectionView.frame = view.bounds
         collectionView.backgroundColor = .white
+        collectionView.collectionViewLayout = createLayout2()
         collectionView.dataSource = self
     }
     
-    static func createLayout() -> UICollectionViewCompositionalLayout {
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
+//            self.collectionView.collectionViewLayout = self.createLayout1()
+            self.collectionView.setCollectionViewLayout(self.createLayout1(), animated: true)
+        })
+    }
+    
+    private func createLayout1() -> UICollectionViewCompositionalLayout {
         // Items
         let itemForGroup1 = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1)))
         itemForGroup1.contentInsets = NSDirectionalEdgeInsets(top: 4, leading: 4, bottom: 4, trailing: 4)
@@ -29,7 +39,7 @@ class ViewController: UIViewController, UICollectionViewDataSource {
         let leftItemForGroup2 = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1)))
         
         let rightItemForGroup2 = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(0.5)))
-        rightItemForGroup2.contentInsets = NSDirectionalEdgeInsets(top: 4, leading: 4, bottom: 4, trailing: 4)
+//        rightItemForGroup2.contentInsets = NSDirectionalEdgeInsets(top: 4, leading: 4, bottom: 4, trailing: 4)
         
         let itemForGroup3 = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1/4), heightDimension: .fractionalHeight(1)))
         itemForGroup3.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
@@ -38,19 +48,55 @@ class ViewController: UIViewController, UICollectionViewDataSource {
         let group1 = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1/3)), subitems: [itemForGroup1])
         
         let group2RightItems = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1)), subitems: [rightItemForGroup2])
+        group2RightItems.interItemSpacing = .fixed(10)
         
         let group2 = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1/3)), subitems: [leftItemForGroup2, group2RightItems])
-        group2.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 0, bottom: 5, trailing: 0)
+//        group2.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 0, bottom: 5, trailing: 0)
+        group2.interItemSpacing = .fixed(10)
         
         let group3 = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1/3)), subitems: [itemForGroup3])
         
-        let mainGroup = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)), subitems: [group1, group2,group3
-                                                                                                                                                    ])
+        let mainGroup = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)), subitems: [group1, group2,group3])
         mainGroup.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10)
         
         // Section
-        let section = NSCollectionLayoutSection(group: mainGroup)
+        let section = NSCollectionLayoutSection(group: group2)
+        section.orthogonalScrollingBehavior = .continuousGroupLeadingBoundary
+        // Return
+        return UICollectionViewCompositionalLayout(section: section)
+    }
+    
+    private func createLayout2() -> UICollectionViewCompositionalLayout {
+        // Items
+        let itemForGroup1 = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1)))
+        itemForGroup1.contentInsets = NSDirectionalEdgeInsets(top: 4, leading: 4, bottom: 4, trailing: 4)
         
+        let leftItemForGroup2 = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1)))
+        
+        let rightItemForGroup2 = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(0.5)))
+//        rightItemForGroup2.contentInsets = NSDirectionalEdgeInsets(top: 4, leading: 4, bottom: 4, trailing: 4)
+        
+        let itemForGroup3 = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1/4), heightDimension: .fractionalHeight(1)))
+        itemForGroup3.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+        
+        // Groups
+        let group1 = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1/3)), subitems: [itemForGroup1])
+        
+        let group2RightItems = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1)), subitems: [rightItemForGroup2])
+        group2RightItems.interItemSpacing = .fixed(10)
+        
+        let group2 = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1/3)), subitems: [leftItemForGroup2, group2RightItems])
+//        group2.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 0, bottom: 5, trailing: 0)
+        group2.interItemSpacing = .fixed(10)
+        
+        let group3 = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1/3)), subitems: [itemForGroup3])
+        
+        let mainGroup = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)), subitems: [group1, group2,group3])
+        mainGroup.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10)
+        
+        // Section
+        let section = NSCollectionLayoutSection(group: group3)
+        section.orthogonalScrollingBehavior = .continuousGroupLeadingBoundary
         // Return
         return UICollectionViewCompositionalLayout(section: section)
     }
